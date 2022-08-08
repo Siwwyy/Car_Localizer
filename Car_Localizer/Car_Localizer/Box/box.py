fd4170b0aebe3394787d7427506653173f2db48b
raxonpc <tomeczekorlowski1@gmail.com>
2022-08-08 20:21:50 +0200

    Test + argument type in draw_box

--- Car_Localizer/Car_Localizer/Box/box.py
+++ Car_Localizer/Car_Localizer/Box/box.py
@@ -1,5 +1,5 @@
 import cv2
 
 
-def draw_box(image, x, y, width, height):
+def draw_box(image, x: int, y: int, width: int, height: int):
     cv2.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), thickness=1)
--- Car_Localizer/Car_Localizer/Box/test.py
+++ Car_Localizer/Car_Localizer/Box/test.py
@@ -2,14 +2,25 @@ from box import draw_box
 import torch
 import cv2
 import torchvision.transforms as transforms
+import numpy
+from PIL import Image
 
 image = cv2.imread("car.jpeg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # Define a transform to convert the image to tensor
-transform = transforms.ToTensor()
+transform_tensor = transforms.ToTensor()
 
 # Convert the image to PyTorch tensor
-tensor = transform(image)
+tensor = transform_tensor(image)
 
-draw_box(tensor, 200, 200, 200, 200)
+transform_pil = transforms.ToPILImage()
+
+pil_img = transform_pil(tensor)
+
+data = numpy.array(pil_img)
+
+draw_box(data, 256, 1148, 1400, 752)
+
+img = Image.fromarray(data, "RGB")
+img.show()
