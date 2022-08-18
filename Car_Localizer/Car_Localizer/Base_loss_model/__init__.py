a56ff0d03980f378e1628b4365a2da6c6558e9e1
PaulWask <pawel.waskiewicz98@gmail.com>
2022-08-18 22:45:26 +0200

    Changing names of the files

--- Car_Localizer/Car_Localizer/Base_loss_model/__init__.py
+++ Car_Localizer/Car_Localizer/Base_loss_model/__init__.py
@@ -1,2 +1,3 @@
 from Base_loss_model.loss_model_base import Loss_Base
-from Base_loss_model.loss_model_first import Loss_First
+from Base_loss_model.loss_mse import Loss_MSE
+from Base_loss_model.loss_mae import Loss_MAE
--- Car_Localizer/Car_Localizer/Base_loss_model/loss_mse.py
+++ Car_Localizer/Car_Localizer/Base_loss_model/loss_mse.py
@@ -1,12 +1,10 @@
 from Base_loss_model.loss_model_base import Loss_Base, TensorType
-import torch.nn as nn
-import numpy as np
 
 
-class Loss_First(Loss_Base):
+class Loss_MSE(Loss_Base):
     def __init__(
         self,
-        name: str = "Loss_First",
+        name: str = "Loss_MSE",
     ):
         super().__init__(name)
 
