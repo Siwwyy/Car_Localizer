d51f016ba13cc4136b9975a23bb3e6b8a94e352a
PaulWask <pawel.waskiewicz98@gmail.com>
2022-08-18 22:45:56 +0200

    Adding Loss MAE fucntion

---
+++ Car_Localizer/Car_Localizer/Base_loss_model/loss_mae.py
@@ -0,0 +1,13 @@
+from Base_loss_model.loss_model_base import Loss_Base, TensorType
+
+
+class Loss_MAE(Loss_Base):
+    def __init__(
+        self,
+        name: str = "Loss_MAE",
+    ):
+        super().__init__(name)
+
+    def forward(self, x: TensorType = None, y_pred: TensorType = None) -> TensorType:
+        output = abs(y_pred - x).mean()
+        return output
