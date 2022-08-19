ba83afdfdf07d3e16f5d7ef0ba961e050e5f634a
PaulWask <pawel.waskiewicz98@gmail.com>
2022-08-19 13:01:35 +0200

    Importing 'abs' directly from torch library

--- Car_Localizer/Car_Localizer/Base_loss_model/loss_mae.py
+++ Car_Localizer/Car_Localizer/Base_loss_model/loss_mae.py
@@ -1,4 +1,5 @@
 from Base_loss_model.loss_model_base import Loss_Base, TensorType
+import torch
 
 
 class Loss_MAE(Loss_Base):
@@ -9,5 +10,5 @@ class Loss_MAE(Loss_Base):
         super().__init__(name)
 
     def forward(self, x: TensorType = None, y_pred: TensorType = None) -> TensorType:
-        output = abs(y_pred - x).mean()
+        output = torch.abs(y_pred - x).mean()
         return output
